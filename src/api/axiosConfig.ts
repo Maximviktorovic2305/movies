d89fd28be3bb3f70a86040aab8d48f3442663d7e
Maximviktorovic2305 <Maximviktorovic123@ya.ru>
2024-08-12 13:45:44 +1000

    fine

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const instanse = axios.create({
+	// baseURL: 'http://85.143.216.62:9000/api/',
 	baseURL: 'http://localhost:9000/api/',
 })
--- src/pages/MovieDetails.vue
+++ src/pages/MovieDetails.vue
@@ -9,15 +9,23 @@
 
 	const route = useRoute()
 	const movie = ref<MovieProps>()
+	const loader = ref<boolean>()
 
 	onMounted(async () => {
-		const response = await instanse.get(`movie/${route.params.id}`)
-		movie.value = await response.data.data
+		try {
+			loader.value = true
+			const response = await instanse.get(`movie/${route.params.id}`)
+			movie.value = await response.data.data
+		} catch (error) {
+			console.log(error)
+		} finally {
+			loader.value = false
+		}
 	})
 </script>
 
 <template>
-	<div v-if="!movie"><Loader /></div>
+	<div v-if="loader"><Loader /></div>
 	<MovieCard v-if="movie" :movie="movie" type="movieDetails" />
 
 	<div v-else class="back-to-home">
@@ -30,6 +38,7 @@
 		<Separator />
 
 		<div class="not-found-block">
+			<Loader v-if="loader" />
 			К сожалению, по вашему запросу ничего не найдено...
 		</div>
 	</div>
